c5642398ea1c6f9261c42b2dc4d60d58ddaa6542
thakuratul2 <pratapsinghatul111@gmail.com>
2023-10-18 22:17:18 +0530

    Atul Pratap Singh Job

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+
+
+<footer>
+  
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 text-right">
+        
+        </div>
+        <div class="col-md-6 col-md-pull-6">
+          <p class="copy-right">Copyright &copy; 2023 Atul Pratap Singh. All Rights Reserved</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
